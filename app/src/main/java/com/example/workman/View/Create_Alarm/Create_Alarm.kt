5da45480743b5111428e88ef78987b8457b703e0
rain2353 <vvkkvv@naver.com>
2020-02-02 14:45:36 +0900

    알람을 생성한다.

--- app/src/main/java/com/example/workman/View/Create_Alarm/Create_Alarm.kt
+++ app/src/main/java/com/example/workman/View/Create_Alarm/Create_Alarm.kt
@@ -1,5 +1,7 @@
 package com.example.workman.View.Create_Alarm
 
+import Alarm
+import android.app.Activity
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
@@ -11,11 +13,8 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
-import android.view.MotionEvent
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.*
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.workman.R
@@ -30,17 +29,59 @@ class Create_Alarm : AppCompatActivity() {
     lateinit var mRtCurrent: Ringtone
     // 문제 해결을 위해 Ringtone으로 변경
     lateinit var mRtm: RingtoneManager
-
     lateinit var am: AlarmManager
     lateinit var tp: TimePicker
     lateinit var con : Context
     var hour: Int = 0
     var min: Int = 0
     lateinit var pi: PendingIntent
+    private lateinit var alarm :Alarm
 
+    private var timePicker :TimePicker? = null
+    private var applyButton :Button? = null
+    private val checkBoxDays = ArrayList<CheckBox>()
+
+    companion object {
+        const val ALARM_PARAM = "alarm"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_alarm)
+        bindViews()
+        intent.extras?.let {
+            alarm = it.getSerializable(ALARM_PARAM) as Alarm
+            setTimepickerTime(alarm.hour,alarm.minute,timePicker)
+            for(t in alarm.days zip checkBoxDays ){
+                t.second.isChecked = t.first
+            }
+            if (alarm.melody != null){
+                mRtCurrent = RingtoneManager.getRingtone(this, Uri.parse(alarm.melody))
+                alarm_sound.text = alarm.melody_name
+            }
+
+        }
+        // day names
+        for(t in checkBoxDays zip com.example.workman.View.Alarm.AlarmFragment.weekdays){
+            t.first.text = t.second
+        }
+        applyButton?.setOnClickListener {
+
+            //set data
+            val hourNminute = getTimepickerTime(timePicker)
+            alarm.hour = hourNminute.first
+            alarm.minute = hourNminute.second
+            alarm.melody = mRtCurrent.toString()
+            alarm.melody_name = mRtCurrent.getTitle(this)
+            for((i,box) in checkBoxDays.withIndex()){
+                alarm.days[i]=box.isChecked
+            }
+
+            // send data back
+            val resultIntent = Intent()
+            resultIntent.putExtra(ALARM_PARAM, alarm)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        }
         // 툴바 설정하기
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -51,53 +92,54 @@ class Create_Alarm : AppCompatActivity() {
         mRtm = RingtoneManager(this)
 
 
-        this.con = this
-        am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        tp = findViewById<TimePicker>(R.id.alarm_timepicker)
-        var calendar : Calendar = Calendar.getInstance()
-        var myIntent: Intent = Intent(this,AlarmReceiver::class.java)
-        button_make_alarm.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    calendar.set(Calendar.HOUR_OF_DAY,tp.hour)
-                    calendar.set(Calendar.MINUTE,tp.minute)
-                    calendar.set(Calendar.SECOND,0)
-                    calendar.set(Calendar.MILLISECOND,0)
-                    hour = tp.hour
-                    min = tp.minute
-                }else{
-                    calendar.set(Calendar.HOUR_OF_DAY,tp.currentHour)
-                    calendar.set(Calendar.MINUTE,tp.currentMinute)
-                    calendar.set(Calendar.SECOND,0)
-                    calendar.set(Calendar.MILLISECOND,0)
-                    hour = tp.currentHour
-                    min = tp.currentMinute
-                }
-                var hr_str: String = hour.toString()
-                var min_str: String = min.toString()
-                if (hour > 12){
-                    hr_str = (hour - 12).toString()
-                }
-                if (min < 10){
-                    min_str = "0$min"
-                }
-                Toast.makeText(con,"Alarm set to: $hr_str : $min_str",Toast.LENGTH_SHORT).show()
-                myIntent.putExtra("extra","on")
-                pi = PendingIntent.getBroadcast(this@Create_Alarm,0,myIntent,PendingIntent.FLAG_UPDATE_CURRENT)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    am.setExact(AlarmManager.RTC_WAKEUP,calendar.timeInMillis,pi)
-                }
 
-            }
-
-        })
-        button_cancel_alarm.setOnClickListener{
-            Toast.makeText(con,"Alarm off",Toast.LENGTH_SHORT).show()
-            pi = PendingIntent.getBroadcast(this@Create_Alarm,0,myIntent,PendingIntent.FLAG_UPDATE_CURRENT)
-            am.cancel(pi)
-            myIntent.putExtra("extra","off")
-            sendBroadcast(myIntent)
-        }
+//        this.con = this
+//        am = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//        tp = findViewById<TimePicker>(R.id.alarm_timepicker)
+//        var calendar : Calendar = Calendar.getInstance()
+//        var myIntent: Intent = Intent(this,AlarmReceiver::class.java)
+//        button_make_alarm.setOnClickListener(object : View.OnClickListener{
+//            override fun onClick(v: View?) {
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                    calendar.set(Calendar.HOUR_OF_DAY,tp.hour)
+//                    calendar.set(Calendar.MINUTE,tp.minute)
+//                    calendar.set(Calendar.SECOND,0)
+//                    calendar.set(Calendar.MILLISECOND,0)
+//                    hour = tp.hour
+//                    min = tp.minute
+//                }else{
+//                    calendar.set(Calendar.HOUR_OF_DAY,tp.currentHour)
+//                    calendar.set(Calendar.MINUTE,tp.currentMinute)
+//                    calendar.set(Calendar.SECOND,0)
+//                    calendar.set(Calendar.MILLISECOND,0)
+//                    hour = tp.currentHour
+//                    min = tp.currentMinute
+//                }
+//                var hr_str: String = hour.toString()
+//                var min_str: String = min.toString()
+//                if (hour > 12){
+//                    hr_str = (hour - 12).toString()
+//                }
+//                if (min < 10){
+//                    min_str = "0$min"
+//                }
+//                Toast.makeText(con,"Alarm set to: $hr_str : $min_str",Toast.LENGTH_SHORT).show()
+//                myIntent.putExtra("extra","on")
+//                pi = PendingIntent.getBroadcast(this@Create_Alarm,0,myIntent,PendingIntent.FLAG_UPDATE_CURRENT)
+//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//                    am.setExact(AlarmManager.RTC_WAKEUP,calendar.timeInMillis,pi)
+//                }
+//
+//            }
+//
+//        })
+//        button_cancel_alarm.setOnClickListener{
+//            Toast.makeText(con,"Alarm off",Toast.LENGTH_SHORT).show()
+//            pi = PendingIntent.getBroadcast(this@Create_Alarm,0,myIntent,PendingIntent.FLAG_UPDATE_CURRENT)
+//            am.cancel(pi)
+//            myIntent.putExtra("extra","off")
+//            sendBroadcast(myIntent)
+//        }
         // 원하는 알람을 체크했을때 다른 알람 아이템들 화면에 나오게한다.
         checkBox_Day_of_week_repeat.setOnClickListener {
             if (checkBox_Day_of_week_repeat.isChecked) {
@@ -165,11 +207,45 @@ class Create_Alarm : AppCompatActivity() {
 
         }
 
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+//        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
 
     }
+    private fun getTimepickerTime(timePicker: TimePicker?):Pair<Int,Int>{
+        return if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Pair(timePicker!!.hour,timePicker.minute)
+        }else{
+            Pair(timePicker!!.currentHour, timePicker.currentMinute)
+        }
+    }
 
+    private fun setTimepickerTime(hour:Int, minute:Int, timePicker: TimePicker?){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            timePicker!!.hour=hour
+            // after first access nullable object is successful, no need to call ?x operator (maybe using smart cast)
+            timePicker.minute = minute
+
+        }else{
+            timePicker!!.currentHour = hour
+            timePicker.currentMinute = minute
+        }
+    }
+    private fun bindViews() {
+        timePicker = findViewById(R.id.alarm_timepicker)
+        timePicker?.setIs24HourView(true)
+
+        applyButton = findViewById(R.id.button_make_alarm)
+
+        checkBoxDays.addAll(arrayListOf(
+            findViewById(R.id.check_day_0),
+            findViewById(R.id.check_day_1),
+            findViewById(R.id.check_day_2),
+            findViewById(R.id.check_day_3),
+            findViewById(R.id.check_day_4),
+            findViewById(R.id.check_day_5),
+            findViewById(R.id.check_day_6)
+        ))
+    }
 
 
     // 툴바 뒤로가기 버튼 설정
