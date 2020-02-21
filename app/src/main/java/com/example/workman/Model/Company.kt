f2833246a3caf6341046ffef6f606464a309275e
rain2353 <vvkkvv@naver.com>
2020-02-21 16:12:17 +0900

    회사 모델

---
+++ app/src/main/java/com/example/workman/Model/Company.kt
@@ -0,0 +1,5 @@
+package com.example.workman.Model
+
+class Company(var company_name : String? , var people_num : Int, val cp_lat : String?, val cp_lng : String?, var company_address: String, val owner_uid : String?, val company_code : String ,var cp_group : String?) {
+
+}
