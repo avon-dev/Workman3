d494b304d6adffd4cc7810ac3b816a9cd46edfd4
rain2353 <vvkkvv@naver.com>
2020-02-21 16:12:37 +0900

    네트워크

--- app/src/main/java/com/example/workman/Network/Common.kt
+++ app/src/main/java/com/example/workman/Network/Common.kt
@@ -1,10 +1,11 @@
 package com.example.workman.Network
 
+import com.example.workman.Model.Company
 import com.example.workman.Model.User
 
 object Common {
     var select_user: User? = null
-
+    var create_company: Company? = null
     val registerapi: ServiceApi
         get() {
             val retrofit = RetrofitClient.instance
--- app/src/main/java/com/example/workman/Network/ServiceApi.kt
+++ app/src/main/java/com/example/workman/Network/ServiceApi.kt
@@ -24,4 +24,17 @@ interface ServiceApi {
     @FormUrlEncoded
     fun loginUser(
     )
+
+    @POST("create_company")
+    @FormUrlEncoded
+    fun Create_Company(
+        @Field("company_name") company_name:String?,
+        @Field("people_num") people_num:String?,
+        @Field("cp_lat") cp_lat: String?,
+        @Field("cp_lng") cp_lng:String?,
+        @Field("company_address") company_address : String?,
+        @Field("owner_uid") owner_uid : String?,
+        @Field("company_code") company_code : String?,
+        @Field("cp_group") cp_group : String?
+    ):Observable<String>
 }
